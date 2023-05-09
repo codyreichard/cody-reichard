2d76d6a3ae3bf38dd5199620d6ff78ebda13f3e5
Cody Reichard <codyreichard31@gmail.com>
2023-05-08 20:08:16 -0400

    Adding more detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material.module';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
